acdfe1896389c6b9945980305cbad1bd82f2b322
MokshMalde <maldemoksh437@gmail.com>
2024-06-20 09:01:47 +0530

    project file for printing pattern of java

--- Projects01/projects08.java
+++ Projects01/projects08.java
@@ -2,8 +2,47 @@ package Projects01;
 
 public class projects08 {
 	public static void main(String[] args) {
-		int a = 34;
-		int result = a / 112;
-		System.out.println(result);
+		int n = 5;
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || i == j) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			for (int j = 0; j <= 2 * (n - i); j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || j == i) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+		for (int i = n; i >= 0; i--) {
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || i == j) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			for (int j = 0; j <= 2 * (n - i); j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || j == i) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 	}
 }
