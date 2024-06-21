d31643d21740abbe2fc17c9025a42ba1fa7196a5
MokshMalde <maldemoksh437@gmail.com>
2024-06-21 22:13:54 +0530

    function project no.2 in java

---
+++ Projects01/functions/functions02.java
@@ -0,0 +1,23 @@
+package Projects01.functions;
+
+public class functions02 {
+	public static boolean isPrime(int n) {
+		if (n <= 1) {
+			return false;
+		}
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		int n = 4;
+		if (isPrime(n)) {
+			System.out.println(n + " is a prime number");
+		} else
+			System.out.println(n + " is not a prime number");
+	}
+}
