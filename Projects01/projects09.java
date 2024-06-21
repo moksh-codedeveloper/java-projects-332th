30407abfb392b4e5c56f0a779f7d6739dac9eb3f
MokshMalde <maldemoksh437@gmail.com>
2024-06-21 21:22:58 +0530

    pattern no 9 of java completed

--- Projects01/projects09.java
+++ Projects01/projects09.java
@@ -3,17 +3,18 @@ package Projects01;
 public class projects09 {
 	public static void main(String[] args) {
 		int n = 4;
-		for (int i = 0; i <= n; i++) {
-			for (int j = i; j >= 0; j--) {
-				System.out.print("*");
-			}
-
-			for (int j = 0; j <= 2 * (n - i); j++) {
+		for (int i = 1; i <= n; i++) {
+			// space
+			for (int j = 1; j <= n - i; j++) {
 				System.out.print(" ");
 			}
-
+			// first part
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j + " ");
+			}
+			// second part
 			for (int j = 2; j <= i; j++) {
-				System.out.print("*");
+				System.out.print(j + " ");
 			}
 			System.out.println();
 		}
