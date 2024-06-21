4b4c457ba9c253546a1317cf745f8626fb18b395
MokshMalde <maldemoksh437@gmail.com>
2024-06-21 21:53:08 +0530

    pattern no 10 of java completed

---
+++ Projects01/patterns10.java
@@ -0,0 +1,17 @@
+package Projects01;
+
+public class patterns10 {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 0; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
