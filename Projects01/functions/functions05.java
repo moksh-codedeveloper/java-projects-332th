1f5a4a71833527b907b65b1c1cc2185090ecbe28
MokshMalde <maldemoksh437@gmail.com>
2024-06-23 13:05:31 +0530

    function 05 of java completed

---
+++ Projects01/functions/functions05.java
@@ -0,0 +1,20 @@
+package Projects01.functions;
+
+public class functions05 {
+	public static int twoGcd(int x, int y) {
+		int gcd = 1;
+		for (int i = 1; i <= x && i <= y; i++) {
+			if (x % i == 0 && y % i == 0) {
+				gcd = i;
+			}
+		}
+
+		return gcd;
+	}
+
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 8;
+		System.out.println(twoGcd(a, b));
+	}
+}
