3345ed5d32b5974f02539ae88177adc92be1261b
MokshMalde <maldemoksh437@gmail.com>
2024-06-23 12:34:15 +0530

    function 04 of java completed

---
+++ Projects01/functions/function04.java
@@ -0,0 +1,20 @@
+package Projects01.functions;
+
+public class function04 {
+	static double power(double x, int n) {
+		if (n == 0) {
+			return 1;
+		} else if (n > 0) {
+			return x * power(x, n - 1);
+		} else {
+			return 1 / power(x, -n);
+		}
+	}
+
+	public static void main(String[] args) {
+		double x = 3.5;
+		int n = -2;
+		System.out.println(power(x, n));
+	}
+
+}
