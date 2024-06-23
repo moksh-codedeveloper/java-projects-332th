27db86a9449fb84f953afe23037b2ae5988426bf
MokshMalde <maldemoksh437@gmail.com>
2024-06-23 13:13:23 +0530

    function 06 of java completed

---
+++ Projects01/functions/function06.java
@@ -0,0 +1,22 @@
+package Projects01.functions;
+
+public class function06 {
+	// Function to display Fibonacci series up to n terms
+	static void displayFibonacci(int n) {
+		int first = 0, second = 1, next;
+		System.out.print("Fibonacci series up to " + n + " terms: ");
+		for (int i = 0; i < n; ++i) {
+			System.out.print(first + ", ");
+			next = first + second;
+			first = second;
+			second = next;
+		}
+	}
+
+	public static void main(String[] args) {
+		// Given number n
+		int n = 10;
+		// Function call
+		displayFibonacci(n);
+	}
+}
